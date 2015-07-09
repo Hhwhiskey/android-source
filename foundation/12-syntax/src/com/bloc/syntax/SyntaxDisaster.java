5c3e7d62d39064f33ccd666aed2a5ed23b90f434
Kevin <khfire22@gmail.com>
2015-07-09 18:17:47 -0400

    Syntax

--- foundation/12-syntax/src/com/bloc/syntax/SyntaxDisaster.java
+++ foundation/12-syntax/src/com/bloc/syntax/SyntaxDisaster.java
@@ -8,17 +8,21 @@ public class SyntaxDisaster extends Object {
 	 *	Fix the code found below this block
 	/************************************************/
 
-	public static void main(String [] args) {;}
-		if (true); {
-			int x = 5
+	public static void main(String [] args) {
+		int x = 5;
+		if (x == 5) {
+			System.out.println("If you can see this message, you've won!");
+		
 		}
-		int syntaxIsFun; 5;
+		
+	}
+}	
 		/* The following line prints stuff out, I promise
 		System.out.println("If you can see this message, you've won!);
-	}
+
 
 	/************************************************
 	 *	ASSIGNMENT:
 	 *	Fix the code found above this block
 	/************************************************/
-}
+
