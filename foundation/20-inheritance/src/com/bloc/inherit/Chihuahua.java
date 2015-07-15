ac6a5cd3b4d5a30dce7343846c408fb75b688968
Kevin <khfire22@gmail.com>
2015-07-15 16:40:12 -0400

    20-Inheritance

--- foundation/20-inheritance/src/com/bloc/inherit/Chihuahua.java
+++ foundation/20-inheritance/src/com/bloc/inherit/Chihuahua.java
@@ -4,6 +4,19 @@ package com.bloc.inherit;
  *	ASSIGNMENT:
  *	Define the Chihuahua class below
  *
- *	Chiuahuas must be fed 5 times in order to grow
+ *	Chihuahuas must be fed 5 times in order to grow
  *	larger.
 /************************************************/
+public class Chihuahua extends Dog {
+	
+	@Override	
+	public void feed() {
+		
+		mWeight += WEIGHT_GAINED_FROM_FEEDING;
+		// Pre-increment feed counter
+		if (++mFeedCounter == 5) {
+			changeSize(true);
+			mFeedCounter = 0;
+		}
+	}
+}
